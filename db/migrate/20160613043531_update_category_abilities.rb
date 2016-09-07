8231ed63eaecb7f688109edb5f3dfe088c0496c5
Scott Willson <scott.willson@gmail.com>
2016-09-07 13:40:16 -0700

    Just use ::Category.update_all_from_names! in migration

--- db/migrate/20160613043531_update_category_abilities.rb
+++ db/migrate/20160613043531_update_category_abilities.rb
@@ -1,14 +1,5 @@
 class UpdateCategoryAbilities < ActiveRecord::Migration
   def change
-    ::Category.transaction do
-      Category.all.each do |category|
-        category.set_abilities_from_name
-        category.set_ages_from_name
-        category.set_equipment_from_name
-        category.set_gender_from_name
-        category.set_weight_from_name
-        category.save!
-      end
-    end
+    ::Category.update_all_from_names!
   end
 end
