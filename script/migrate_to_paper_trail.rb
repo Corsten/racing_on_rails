20bdd4d214e355c5153ffeb569d03c414153d051
Scott Willson <scott.willson@gmail.com>
2018-01-22 13:46:49 -0800

    Handle all VestalVersions to PaperTrail migration error cases

--- script/migrate_to_paper_trail.rb
+++ script/migrate_to_paper_trail.rb
@@ -1,12 +1,7 @@
-PaperTrailVersion.delete_all
+def remove_old_attributes
+  count = versions_query.count
+  index = 0
 
-versions_query = VestalVersions::Version
-    .where(versioned_type: ["DiscountCode", "Event", "SingleDayEvent", "MultidayEvent", "Series", "WeeklySeries"])
-
-count = versions_query.count
-index = 0
-
-PaperTrailVersion.transaction do
   puts "Remove old attributes from versions"
   versions_query.find_each do |version|
     index = index + 1
@@ -14,28 +9,58 @@ PaperTrailVersion.transaction do
       puts "#{index}/#{count}"
     end
 
+    modifications = version.modifications
+
     case version.versioned_type
     when "DiscountCode"
-      modifications = version.modifications
       modifications.delete("status")
       modifications.delete("use_for")
       version.update_attributes! modifications: modifications
     when "Event", "SingleDayEvent", "MultidayEvent", "Series", "WeeklySeries"
-      modifications = version.modifications
       modifications.delete("notification")
       modifications.delete("lock_version")
       version.update_attributes! modifications: modifications
-    when "Page"
-      modifications = version.modifications
+    when "Page", "RaceNumber", "Team"
       modifications.delete("lock_version")
+      version.update_attributes! modifications: modifications
+    when "Person"
+      modifications.delete("card_type")
+      modifications.delete("created_by_id")
+      modifications.delete("fullname")
+      modifications.delete("created_by_type")
+      modifications.delete("last_updated_by")
+      version.update_attributes! modifications: modifications
+    when "Refund"
+      modifications.delete("order_id")
       version.update_attributes! modifications: modifications
     end
   end
+end
+
+def versions_query
+  VestalVersions::Version
+    .where(versioned_type: %w[
+            DiscountCode
+            Event
+            MultidayEvent
+            Page
+            Person
+            RaceNumber
+            Refund
+            Series
+            SingleDayEvent
+            Team
+            WeeklySeries
+         ])
+end
+
+PaperTrailVersion.delete_all
+
+PaperTrailVersion.transaction do
+  remove_old_attributes
 
   puts
   puts "Copy versions to PaperTrail"
-  count = VestalVersions::Version.count
-  index = 0
   [
     DiscountCode,
     Event,
@@ -46,44 +71,58 @@ PaperTrailVersion.transaction do
     Refund,
     Team
   ].each do |record_class|
+    count = record_class.count
+    index = 0
     puts
     puts record_class
-    record_class.find_each do |record|
-      index = index + 1
-      if index % 1000 == 0
-        puts "#{index}/#{count}"
-      end
+    record_class.where(id: VestalVersions::Version.pluck(:versioned_id)).find_each do |record|
+      index += 1
+      puts("#{index}/#{count}") if index % 1000 == 0
+
       versions = record.versions
       max_version = versions.size - 1
 
-      (0..max_version).each do |index|
+      (0..max_version).each do |version_index|
         begin
-          version = versions[index]
-          record.reload.revert_to index
+          version = versions[version_index]
+          record.reload.revert_to version_index
+
+          unless record.id
+            puts "No ID for #{record_class} version #{version_index} version ID #{version.id}"
+            next
+          end
 
           attributes = record.attributes.dup
+          attributes.delete(:card_type)
+          attributes.delete(:created_by_id)
+          attributes.delete(:created_by_type)
+          attributes.delete(:fullname)
+          attributes.delete(:last_updated_by)
           attributes.delete(:lock_version)
           attributes.delete(:notification)
+          attributes.delete(:order_id)
           attributes.delete(:use_for)
 
-          if record.is_a?(DiscountCode)
-            attributes.delete(:status)
-          end
+          attributes.delete(:status) if record.is_a?(DiscountCode)
 
           PaperTrailVersion.create!(
             created_at: version.updated_at,
-            event: index == 0 ? "create" : "update",
+            event: version_index == 0 ? "create" : "update",
             item_type: version.versioned_type,
             item_id: record.id,
             object: attributes.to_yaml,
             whodunnit: record.updated_by_person_name
           )
         rescue ActiveModel::MissingAttributeError => e
-          puts "#{e} for #{record_class} ID #{record.id} version #{index}"
+          puts "#{e} for #{record_class} ID #{record.id} version #{version_index} version ID #{version.id}"
           pp attributes
           raise e
+        rescue ActiveRecord::RecordNotFound => e
+          puts "#{e} for #{record_class} ID #{record.id} version #{version_index} version ID #{version.id}"
+          pp attributes
+          next
         rescue StandardError => e
-          puts "Could not migrate #{record_class} ID #{record.id} version #{index}"
+          puts "Could not migrate #{record_class} ID #{record.id} version #{version_index} version ID #{version.id}"
           pp version
           raise e
         end
